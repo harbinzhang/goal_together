edff87b843de425775943de5fc8ee53443abb3ca
Haibin Zhang <haibinzh@haibinzh-macbookpro.roam.corp.google.com>
2020-03-22 16:22:18 -0700

    added tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -10,27 +10,52 @@ import 'package:goaltogether/models/Record.dart';
 import 'package:goaltogether/pages/UsersPage.dart';
 import 'package:goaltogether/pages/HabitsPage.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(TabBarDemo());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class TabBarDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          bottomNavigationBar: menu(),
+          body: TabBarView(
+            children: [
+              UsersPage(title: 'Welcome'),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
       ),
-      home: UsersPage(title: 'haibin'),
+    );
+  }
+
+  Widget menu() {
+    return Container(
+//      color: Color(0xFF3F5AA6),
+      child: TabBar(
+        labelColor: Colors.blueGrey,
+//        unselectedLabelColor: Colors.white70,
+        indicatorSize: TabBarIndicatorSize.tab,
+//        indicatorPadding: EdgeInsets.all(5.0),
+        indicatorColor: Colors.blue,
+        tabs: [
+          Tab(
+            text: "User",
+            icon: Icon(Icons.supervised_user_circle),
+          ),
+          Tab(
+            text: "Wish",
+            icon: Icon(Icons.poll),
+          ),
+          Tab(
+            text: "More",
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      )
     );
   }
 }
---
+++ lib/pages/WishesPage.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
+
+import 'package:goaltogether/models/Habit.dart';
+import 'package:goaltogether/providers/HabitTableHandler.dart';
+import 'package:goaltogether/res/colors.dart';
+
+import 'package:goaltogether/components/HabitCard.dart';
+
+class HabitsPage extends StatefulWidget {
+  HabitsPage({Key key, this.user}) : super(key: key);
+
+  final String user;
+
+  @override
+  _HabitsPageState createState() => _HabitsPageState(user: this.user);
+}
+
+
+class _HabitsPageState extends State<HabitsPage> {
+  _HabitsPageState({this.user});
+
+  final user;
+  final habitsHandler = HabitTableHandler();
+  List<Habit> _habits = [];
+  final _habitController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        appBar: AppBar(title: Text("$user's Habit"),),
+        body: _buildHabitsList(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            _showAddHabit();
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.green,
+        )
+    );
+  }
+
+  Widget _buildHabitsList() {
+
+    print(_habits);
+    return ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: _habits.length,
+      itemBuilder: (BuildContext context, int index) {
+        final item = _habits[index];
+        var habitName = item.habitName;
+        return Dismissible(
+          background: Container(color: kShrinePink50),
+          key: UniqueKey(),
+          onDismissed: (direction) {
+            setState(() {
+              _delete(item.id);
+              _habits.removeAt(index);
+            });
+
+            Scaffold.of(context)
+                .showSnackBar(SnackBar(content: Text("$item dismissed")));
+          },
+          child: RaisedButton(
+              child: habitCard(habitName),
+              onPressed: () {
+
+              }
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+    );
+  }
+
+  void _refreshHabitsList() async {
+//    var habits = await habitsHandler.queryAllRows();
+    var habits = await habitsHandler.queryAllHabitsByUser(this.user);
+    print(habits);
+    if (!DeepCollectionEquality().equals(habits, _habits)) {
+      setState(() {
+        _habits = habits;
+      });
+    }
+  }
+
+  void _insert(String habitName) async {
+    // row to insert
+    final habit = Habit(
+      habitName: habitName,
+      value: 1,
+      user: this.user,
+    );
+    final id = await habitsHandler.insert(habit);
+    print('inserted row id: $id');
+  }
+
+  void _delete(int id) async {
+    // Assuming that the number of rows is the id for the last row.
+    final rowsDeleted = await habitsHandler.delete(id);
+    print('deleted $rowsDeleted row(s): row $id');
+  }
+
+
+  void _showAddHabit() {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("New Habit"),
+          content: new TextField(
+            controller: _habitController,
+            decoration: InputDecoration(
+//                filled: true,
+              labelText: 'habit name',
+            ),
+          ),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Cancel"),
+              onPressed: () {
+                _habitController.clear();
+                Navigator.of(context).pop();
+              },
+            ),
+            new FlatButton(
+              child: new Text("Add"),
+              onPressed: () {
+                _insert(_habitController.text);
+                _refreshHabitsList();
+                _habitController.clear();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+}
